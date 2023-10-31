f48515d662f6c0ed4510d5e8dc531e0eee075884
WojciechIgnaczak <wojciech.ignaczak@onet.pl>
2023-10-31 09:34:11 +0100

    31.10

--- cw31.10.23/slownik.py
+++ cw31.10.23/slownik.py
@@ -1,6 +1,6 @@
 print("d")
 #stworz slownik
-owoce= {"jablko":"czerwony", "banan":"żółty", "kiwi":"zielony", "pomarancza":"pomaranczowy"}    #klucz:wartość
+#owoce= {"jablko":"czerwony", "banan":"żółty", "kiwi":"zielony", "pomarancza":"pomaranczowy"}    #klucz:wartość
 #for kolor in owoce.values():
    # print(kolor)
 #for owoc in owoce.keys():
@@ -15,4 +15,14 @@ owoce= {"jablko":"czerwony", "banan":"żółty", "kiwi":"zielony", "pomarancza":
 #print(owoce.clear()) #czysci slownik
 #print(owoce.pop("banan")) # wyrzuca bananapo wyswitleniu koloru
 #print(owoce.get("kiwi",0)) #jesli nie ma kiwi w owocach to wartość = 0
+
+
+# slownik1= {"a":1, "b":2}
+# slownik2= {"b":3, "c":4}
+# slownik1.update(slownik2) # a=1, b=3! c=4
+
+
+#slownik2=slownik1
+# slownik2=slownik1.copy()
+# print(slownik2)
 
