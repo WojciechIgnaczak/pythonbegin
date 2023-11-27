377f26ffd3453f97f894adc369c1ec70e9ce769a
WojciechIgnaczak <wojciech.ignaczak@onet.pl>
2023-11-27 17:48:30 +0100

    mn

--- cw7.11.2023/9.py
+++ cw7.11.2023/9.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #8znaków, 1 cyfra, 1 duza litera
 def validate_password(password):
     is_long = len(password)>=8
@@ -9,16 +8,4 @@ def validate_password(password):
 haslo = input("podaj swoje haslo")
 if validate_password(haslo):
     print("mama silne haslo")
-=======
-#8znaków, 1 cyfra, 1 duza litera
-def validate_password(password):
-    is_long = len(password)>=8
-    mam_cyfre = any(char.isdigit() for char in password)
-    mam_wielk_litera = any(char.isupper() for char in password)
-    return mam_cyfre and mam_wielk_litera and is_long
-
-haslo = input("podaj swoje haslo")
-if validate_password(haslo):
-    print("mama silne haslo")
->>>>>>> d0217b5a793324b2c5a364914c7d7deadf1f3470
 else: print("masz za slabe haslo")
